f59e154a85b48f62cc0cbaa5ecff1c8ec934ca59
Jack <Jack@jsy.com>
2021-08-27 16:05:42 +0800

    push

--- 28_图书管理系统(文件操作).py
+++ 28_图书管理系统(文件操作).py
@@ -2,7 +2,7 @@
 #list ，元组，字典 ---->内存里，不属于持久化保存
 #存储数据  D:\测试(test)文件夹\Test1
 #用户注册
-def login():
+def register():
     username = input('输入用户名：')
     password = input('输入密码：')
     repassword = input('确认输入密码：')
@@ -10,5 +10,34 @@ def login():
     if password == repassword:
         #保存信息
         with open(r'D:\测试(test)文件夹\Test1\user.txt','a') as wstream:
-            wstream.write('{} {}'.format(username,password))
+            wstream.write('{} {}\n'.format(username,password))
+            # wstream.writelines([username+' ',password+'\n'])
+        print('注册成功！')
+    else:
+        print('两次输入密码不一致，请再次尝试注册！')
+        register()
+def login():
+    username = input('输入用户名：')
+    password = input('输入密码：')
 
+    if username and password:
+        with open(r'D:\测试(test)文件夹\Test1\user.txt','r') as rstream:
+            while True:
+                user = rstream.readline()
+                input_user = '{} {}\n'.format(username,password)
+
+                if input_user==user:
+                    break
+            else:
+                print('用户名或密码错误，请重新登录！')
+                login()
+def show_books():
+    print('------图书馆里的书有------')
+    with open(r'D:\测试(test)文件夹\Test1\books.txt',encoding="utf-8") as  rstream:
+        booklist = rstream.readlines()
+        for book in booklist:
+            print(book,end='')
+
+# register()
+# login()
+show_books()
