a75f6425e691b4d639b61c099b31dc4a4628e48d
Jack <Jack@jsy.com>
2021-08-30 12:11:17 +0800

    push

--- 31_生成器和迭代器.py
+++ 31_生成器和迭代器.py
@@ -10,6 +10,7 @@
 1.通过列表推导式得到生成器，外部是括号
 2.借助函数完成，关键字 yield
 
+应用：协程方面
 '''
 newlist = [x + 1 for x in range(10)]
 print(type(newlist))
@@ -87,17 +88,59 @@ print(next(g))
 print(next(g))
 print(next(g))
 print(next(g))
-print(next(g))
+# print(next(g))
 
 '''
-
+next()     系统方法，产生下一个元素，若生成器元素迭代完之后，再调用会报错
+生成器内部方法：
+_next()_  获取下一个元素，若生成器元素迭代完之后，再调用会报错
+send(value):向每次生成器调用中传值  注意：第一次使用生成器时，不能向生成器传入非空的值(必须空值)
+             在生成元素的同时还往生成器里传值，同时根据传入的值在生成器内部完成一系列动作
 '''
 def gen():
     i = 0
     while i<5:
-        temp =yield i
+        temp =yield i   #先执行关键字操作，返回i，暂停代码；下次再执行时，send给yield一个返回值'你好'
         print('temp:',temp)
         i +=1
     return '没有更多的数据！'
 
 g = gen()
+# print(next(g))
+# print(next(g))
+# print(next(g))
+#
+# print(g.__next__())
+
+print(g.send(None))
+n = g.send('你好')  #第一次使用生成器时，不能向生成器传入非空的值(必须空值)
+print(n)
+
+'''
+进程 > 线程 > 协程
+生成器应用：在协程中使用，多任务间的切换
+#例如：迅雷下载文件，可将文件切块分配给多个线程下载，从而使下载速度加快
+         当一个线程里有多任务时，引入协程
+'''
+def task1(n):
+    for i in range(n):
+        print('正在下载第{}个”文件“'.format(i))
+        yield None
+
+def task2(n):
+    for i in range(n):
+        print('正在吃第%s碗饭'%(i))
+        yield None
+
+# task1(5)
+# task2(5)
+#实现函数打印顺序交替执行
+g1 = task1(5)
+g2 = task2(5)
+while True:
+    try:
+        g1.__next__()
+        g2.__next__()
+    except:
+        break
+
