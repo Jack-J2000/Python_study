332534fe03f3dd64e2d1f29c61e50cf5ff6c057e
Jack <2992075490@qq.com>
2021-09-20 23:27:00 +0800

    push

--- 54_进程和线程回顾.py
+++ 54_进程和线程回顾.py
@@ -19,4 +19,27 @@ p.start()
 #进程的数据共享
 n = 0  进程对于变量n，每个进程都有自己的n，互不干扰
  
+ #进程池：Pool
+ p = Pool(5)
+ 
+ 阻塞式    apply(func,args,kwargs)
+ 非阻塞式  apply_async(func,args,kwargs,callback=进程做完后执行的函数)
+ 
+ #进程间通信
+q = Queue(队列容量)
+q.put()
+q.get()
+q.empty()
+q.full()
+
+#线程：与进程是包含关系
+进程里面可以存着多个线程，多个线程共用进程资源
+t = Thread(target=func,name='',args=(1,),kwargs='')
+t.start()
+GIL：全局解释器锁
+线程：'伪线程'
+
+进程做任务速度更快，线程适合做耗时操作
+
+线程同步：
 '''
---
+++ 56_死锁.py
@@ -0,0 +1,43 @@
+''
+'''
+开发过程中使用线程，在线程间共享多个资源的时候，
+如果两个线程分别占有一部分资源并且同时等待对方的资源，就会造成死锁
+尽管死锁很少发生，但一旦发生就会造成应用的停止响应，程序不做任何事情
+
+避免死锁的方法：
+1.重构代码
+2.在acquire里加timeout=
+'''
+from threading import Lock,Thread
+import time
+
+lockA = Lock()
+lockB = Lock()
+class MyThread(Thread):
+    # def __init__(self,name):
+    #     pass
+
+    def run(self):   #start()
+        if lockA.acquire():   #如果获取到锁则返回Ture
+            print(self.name+'获取了A锁')
+            time.sleep(1)   #线程1在休息的时候B锁就被线程2获取了，所以无法获取B锁
+            if lockB.acquire(timeout=2):   #如果2秒得不到B锁，两秒后释放A锁
+                print(self.name+'又获取了B锁')
+                lockB.release()
+            lockA.release()
+
+class MyThread1(Thread):
+    def run(self):   #start()
+        if lockB.acquire():   #如果获取到锁则返回Ture
+            print(self.name+'获取了B锁')
+            time.sleep(1)
+            if lockA.acquire():
+                print(self.name+'又获取了A锁')
+                lockA.release()
+            lockB.release()
+if __name__ == '__main__':
+    t1 = MyThread()
+    t2 = MyThread1()
+
+    t1.start()
+    t2.start()
